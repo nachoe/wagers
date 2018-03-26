81de589b2cf855464ea28e519a5accfd4ef89480
Jesse <jesse.matlock@gmail.com>
2018-03-26 06:21:37 -0700

    added npm stuff

--- webpack.mix.js
+++ webpack.mix.js
@@ -13,3 +13,4 @@ let mix = require('laravel-mix');
 
 mix.js('resources/assets/js/app.js', 'public/js')
    .sass('resources/assets/sass/app.scss', 'public/css');
+mix.react('resources/assets/js/app.jsx', 'public/js');
